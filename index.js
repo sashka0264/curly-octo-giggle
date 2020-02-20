82d17d1f63eccc4940b26c33e1cfbf05116842c3
Alexandr Kolesnilov <sashka0264@yandex.ru>
2020-02-20 14:52:33 +0300

    ...

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema'); 
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3005;
+
+app.use(cors());
+app.options('*', cors());
 
 app.use('/', graphqlHTTP({ 
   schema, 
   graphiql: true
 }));
+
 
 app.listen(PORT, (err) => {
   err ? console.log(error) : console.log('Server started...');
